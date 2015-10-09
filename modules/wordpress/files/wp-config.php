31ba47a21c65053740f1724e86fa163846e641b0
Travis Nychay <tjnychay@gmail.com>
2015-10-09 14:55:00 -0400

    updating password for wp db

--- modules/wordpress/files/wp-config.php
+++ modules/wordpress/files/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'blog');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rean_test');
+define('DB_PASSWORD', 'test');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
